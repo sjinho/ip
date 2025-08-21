62dd9035f9a3eeb369f8a626a38a19008e9cb684
jinhoson <s.jinho1617@gmail.com>
2025-08-22 00:26:09 +0800

    Level 0 Rename JinBot, greet and exit

---
+++ src/main/java/JinBot.java
@@ -0,0 +1,13 @@
+public class JinBot {
+    public static final String HORIZONTAL_LINE =
+            "____________________________________________";
+
+    public static void main(String[] args) {
+        System.out.println(HORIZONTAL_LINE);
+        System.out.println(" Hello! I'm JinBot");
+        System.out.println(" What can I do for you?");
+        System.out.println(HORIZONTAL_LINE);
+        System.out.println(" Bye. Hope to see you again soon!");
+        System.out.println(HORIZONTAL_LINE);
+    }
+}
