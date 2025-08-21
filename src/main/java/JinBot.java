5d6b382cc35a528c3122f7e7331a6373558c953b
jinhoson <s.jinho1617@gmail.com>
2025-08-22 01:33:12 +0800

    Level 1: Add Echo

--- src/main/java/JinBot.java
+++ src/main/java/JinBot.java
@@ -1,13 +1,35 @@
+import java.util.Scanner;
+
 public class JinBot {
-    public static final String HORIZONTAL_LINE =
-            "____________________________________________";
+    private static final String SPACING = "        ";
+
+    private static final String INDENTATION = "  ";
+
+    private static final String HORIZONTAL_LINE =
+            SPACING + "______________________________________________________";
+
+    private static final String GREET =
+            "Hello! I'm JinBot\n" + SPACING + INDENTATION + "What can I do for you?";
+
+    public static void printMessageBlock(String response) {
+        System.out.println(HORIZONTAL_LINE);
+        System.out.println(SPACING + INDENTATION + response);
+        System.out.println(HORIZONTAL_LINE);
+    }
 
     public static void main(String[] args) {
-        System.out.println(HORIZONTAL_LINE);
-        System.out.println(" Hello! I'm JinBot");
-        System.out.println(" What can I do for you?");
-        System.out.println(HORIZONTAL_LINE);
-        System.out.println(" Bye. Hope to see you again soon!");
-        System.out.println(HORIZONTAL_LINE);
+        JinBot.printMessageBlock(GREET);
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            String userInput = scanner.nextLine();
+
+            if (userInput.equals("bye")) {
+                JinBot.printMessageBlock("Bye. Hope to see you again soon!");
+                break;
+            }
+
+            JinBot.printMessageBlock(userInput);
+        }
     }
 }
